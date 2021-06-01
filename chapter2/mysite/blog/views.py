6fe097acf1ae0cf8efe7623b6f52e644029a68b5
Mojtaba Banaie <mojtaba.banaie@gmail.com>
2021-06-01 22:50:45 +0430

    Tags - V3

--- chapter2/mysite/blog/views.py
+++ chapter2/mysite/blog/views.py
@@ -4,6 +4,7 @@ from .forms import EmailPostForm, CommentForm
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.core.mail import send_mail
 from taggit.models import Tag
+from django.db.models import Count
 
 
 def post_list(request,  tag_slug=None):
@@ -57,12 +58,22 @@ def post_detail(request, year, month, day, post):
 
     post.visits = post.visits + 1
     post.save()
+
+    # List of similar posts
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.objects.filter(
+        tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags'))\
+        .order_by('-same_tags', '-publish')[:4]
+
     return render(request,
                   'blog/post/detail.html',
                   {'post': post,
                    'comments': comments,
                    'new_comment': new_comment,
-                   'comment_form': comment_form})
+                   'comment_form': comment_form,
+                   'similar_posts': similar_posts
+                   })
 
 
 def post_share(request, post_id):
